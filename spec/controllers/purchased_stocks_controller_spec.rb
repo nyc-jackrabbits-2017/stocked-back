24a4613b7cdbf1dbb175febfbd37af65e60fdc75
faralbrt <albert.farhi5@gmail.com>
2017-01-09 15:04:17 -0500

    add purchased_stocks routes

--- spec/controllers/purchased_stocks_controller_spec.rb
+++ spec/controllers/purchased_stocks_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe PurchasedStocksController, type: :controller do
+  let!(:user) {User.create!(first_name: "Albert", last_name: "Farhi", email: "albert@aol.com", password: "password")}
+  let!(:purchased_stock) {user.purchased_stocks.create!(stock_symbol: "ARIA", purchase_price: 23.71, quantity: rand(1..1000))}
 
+  describe "GET #index" do
+    it "responds with a status code of 200"
+  end
 end
