89b5e11dd37b9c0390ba13f14e5e70cc66558543
[01;31m[K"name"[m[K: "Alexandra Cooper <[01;31m[K"email"[m[K: null>
2017-01-09 17:19:03 -0500

    troubleshoot instance methods for quote info

--- app/models/purchased_stock.rb
+++ app/models/purchased_stock.rb
@@ -5,4 +5,33 @@ class PurchasedStock < ApplicationRecord
   def cost_basis
     self.purchase_price * self.quantity
   end
+
+  def company_name
+    portfolio = YahooFinance::Client.new
+    stock = [self.stock_symbol]
+    name = portfolio.quotes(stock, [:name])
+    name[0].name 
+  end 
+
+  def last_trade_date
+    portfolio = YahooFinance::Client.new
+    stock = [self.stock_symbol]
+    date = portfolio.quotes(stock, [:last_trade_date])
+    date[0].last_trade_date
+  end 
+
+  def last_trade_time
+    portfolio = YahooFinance::Client.new
+    stock = [self.stock_symbol]
+    time = portfolio.quotes(stock, [:last_trade_time])
+    time[0].last_trade_time
+  end 
+
+  def last_trade_price
+    portfolio = YahooFinance::Client.new
+    stock = [self.stock_symbol]
+    price = portfolio.quotes(stock, [:last_trade_price])
+    price[0].last_trade_price
+  end 
+
 end
