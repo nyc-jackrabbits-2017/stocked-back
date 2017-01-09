77626fe7abe9ba0387615cfeeded0992d6f0d64d
Alexandra Cooper <email": null>
2017-01-09 14:38:39 -0500

    add seed data

--- app/models/purchased_stock.rb
+++ app/models/purchased_stock.rb
@@ -1,4 +1,8 @@
 class PurchasedStock < ApplicationRecord
   validates_presence_of :purchase_price, :quantity, :stock_symbol
   belongs_to :user
+
+  def cost_basis
+    self.purchase_price * self.quantity
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  User.create!(first_name: Faker::Name.first_name,
+               last_name: Faker::Name.last_name,
+               email: Faker::Internet.email,
+               password: "password")
+end
+
+stock_purchases = [{stock_symbol: "ARIA", purchase_price: 23.71, quantity: rand(1..1000)},
+                  {stock_symbol: "FB", purchase_price: 124.82, quantity: rand(1..1000)},
+                  {stock_symbol: "CBIS", purchase_price: 0.06, quantity: rand(1..1000)},
+                  {stock_symbol: "CBDS", purchase_price: 6.44, quantity: rand(1..1000)},
+                  {stock_symbol: "AAPL", purchase_price: 117.02, quantity: rand(1..1000)}
+                  ]
+
+User.all.each do |user|
+  stocks = stock_purchases.sample(3)
+  stocks.each do |stock|
+    user.purchased_stocks.create!(stock)
+  end
+end
