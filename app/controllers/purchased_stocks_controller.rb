1801b4f98f2f049da46a3cefb59898c3b62f84d0
faralbrt <albert.farhi5@gmail.com>
2017-01-09 16:12:48 -0500

    add PurchasedStocksController#show

--- app/controllers/purchased_stocks_controller.rb
+++ app/controllers/purchased_stocks_controller.rb
@@ -3,4 +3,10 @@ class PurchasedStocksController < ApplicationController
     @user = User.find(params[:user_id])
     render json: @user.purchased_stocks, methods: [:cost_basis]
   end
+
+  def show
+    @purchased_stock = PurchasedStock.find(params[:id])
+    render json: @purchased_stock, methods: [:cost_basis]
+  end
+
 end
--- spec/controllers/purchased_stocks_controller_spec.rb
+++ spec/controllers/purchased_stocks_controller_spec.rb
@@ -13,5 +13,22 @@ RSpec.describe PurchasedStocksController, type: :controller do
       get :index, params: {user_id: user.id}
       expect(JSON.parse(response.body).class).to eq Array
     end
+    it "includes cost_basis in the response" do
+      get :index, params: {user_id: user.id}
+      expect(response.body).to include "cost_basis"
+    end
+  end
+  describe "GET #show" do
+    before (:each) do
+      get :show, params: {user_id: user.id, id: purchased_stock.id}
+    end
+
+    it "responds with a status code of 200" do
+      expect(response).to have_http_status 200
+    end
+
+    it "includes cost_basis in the response" do
+      expect(response.body).to include "cost_basis"
+    end
   end
 end
