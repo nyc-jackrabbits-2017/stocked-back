66e204578b13dde08474a74e6ab12da408e77f1e
faralbrt <albert.farhi5@gmail.com>
2017-01-09 15:13:46 -0500

    get#index test 1 passed

--- app/controllers/purchased_stocks_controller.rb
+++ app/controllers/purchased_stocks_controller.rb
@@ -1,2 +1,6 @@
 class PurchasedStocksController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    render json: @user.purchased_stocks
+  end
 end
--- spec/controllers/purchased_stocks_controller_spec.rb
+++ spec/controllers/purchased_stocks_controller_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe PurchasedStocksController, type: :controller do
   let!(:purchased_stock) {user.purchased_stocks.create!(stock_symbol: "ARIA", purchase_price: 23.71, quantity: rand(1..1000))}
 
   describe "GET #index" do
-    it "responds with a status code of 200"
+    it "responds with a status code of 200" do
+      get :index, params: {user_id: user.id}
+      expect(response).to have_http_status 200
+    end
   end
 end
