c5fb679ffb0ead49c0ddac78855447ff5e01a5e3
Patrick Kolodgy <email": null>
2017-01-10 14:15:12 -0500

    Config api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :users, only: [:show, :create] do
+  resources :users, only: [:show, :create, :update] do
     resources :purchased_stocks, only: [:index, :show]
     get 'last_year_portfolio_performance' => 'portfolio#last_year_portfolio_performance'
+  end
+
+  namespace :api do
+
   end
 end
