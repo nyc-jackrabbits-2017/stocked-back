5dfdb079cf5377fc5afa222e1295efac8dc11028
faralbrt <albert.farhi5@gmail.com>
2017-01-10 09:58:52 -0500

    add api methods to the json

--- app/controllers/purchased_stocks_controller.rb
+++ app/controllers/purchased_stocks_controller.rb
@@ -1,12 +1,12 @@
 class PurchasedStocksController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    render json: @user.purchased_stocks, methods: [:cost_basis]
+    render json: @user.purchased_stocks, methods: [:cost_basis, :ask, :company_name, :last_trade_date, :last_trade_time, :last_trade_price]
   end
 
   def show
     @purchased_stock = PurchasedStock.find(params[:id])
-    render json: @purchased_stock, methods: [:cost_basis]
+    render json: @purchased_stock, methods: [:cost_basis, :ask, :company_name, :last_trade_date, :last_trade_time, :last_trade_price]
   end
 
 end
