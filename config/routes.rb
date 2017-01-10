95a140a35cfb3e9548312f813ec80eb762b311c9
Patrick Kolodgy <email": null>
2017-01-10 14:17:01 -0500

    Update with serializers gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
+
+
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :users, only: [:show, :create, :update] do
-    resources :purchased_stocks, only: [:index, :show]
-    get 'last_year_portfolio_performance' => 'portfolio#last_year_portfolio_performance'
-  end
 
   namespace :api do
-
+    resources :users, only: [:show, :create, :update] do
+      resources :purchased_stocks, only: [:index, :show]
+      get 'last_year_portfolio_performance' => 'portfolio#last_year_portfolio_performance'
+    end
   end
 end
