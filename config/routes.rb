8676184a7522bedb6b797139389ef379f050616a
Patrick Kolodgy <email": null>
2017-01-10 13:45:12 -0500

    Add route for user show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :users, only: [] do
+  resources :users, only: [:show] do
     resources :purchased_stocks, only: [:index, :show]
     get 'last_year_portfolio_performance' => 'portfolio#last_year_portfolio_performance'
   end
