d2f6942086d409ffaedb90d5c43106ba1e3d6f36
Patrick Kolodgy <email": null>
2017-01-10 10:46:37 -0500

    Reformat JSON with user data

--- app/controllers/purchased_stocks_controller.rb
+++ app/controllers/purchased_stocks_controller.rb
@@ -1,12 +1,36 @@
 class PurchasedStocksController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    render json: @user.purchased_stocks, methods: [:cost_basis, :ask, :company_name, :last_trade_date, :last_trade_time, :last_trade_price]
+
+    render json: {
+      user_id: @user.id,
+      user_name: "#{@user.first_name} #{@user.last_name}",
+      portfolio: @user.purchased_stocks, methods: [
+                                                    :cost_basis,
+                                                    :ask,
+                                                    :company_name,
+                                                    :last_trade_date,
+                                                    :last_trade_time,
+                                                    :last_trade_price
+                                                    ]
+                                                  }
   end
 
   def show
+    @user = User.find(params[:user_id])
     @purchased_stock = PurchasedStock.find(params[:id])
-    render json: @purchased_stock, methods: [:cost_basis, :ask, :company_name, :last_trade_date, :last_trade_time, :last_trade_price]
+    render json: {
+      user_id: @user.id,
+      user_name: "#{@user.first_name} #{@user.last_name}",
+      stock_data: @purchased_stock, methods: [
+                                              :cost_basis,
+                                              :ask,
+                                              :company_name,
+                                              :last_trade_date,
+                                              :last_trade_time,
+                                              :last_trade_price
+                                              ]
+                                            }
   end
 
 end
