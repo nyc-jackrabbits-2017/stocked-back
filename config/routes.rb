761e5b738c2f3b115594a14d56e84e5e9e000849
Patrick Kolodgy <email": null>
2017-01-10 13:51:58 -0500

    Add create user endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
-  resources :users, only: [:show] do
+  resources :users, only: [:show, :create] do
     resources :purchased_stocks, only: [:index, :show]
     get 'last_year_portfolio_performance' => 'portfolio#last_year_portfolio_performance'
   end
