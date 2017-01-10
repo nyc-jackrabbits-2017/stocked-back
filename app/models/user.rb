cb1f3f3185cd23b5baabb321802f49e895b42164
faralbrt <albert.farhi5@gmail.com>
2017-01-10 11:18:24 -0500

    User#last_year_portfolio_performance

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,18 @@ class User < ApplicationRecord
 
   # Returns an array of portfolio values of last year's data month by month
   def last_year_portfolio_performance
-
+    last_year_performance_all_stocks = self.purchased_stocks.reduce([]) do |result, stock|
+      result << stock.last_years_performance
+    end
+    sum_for_each_month(last_year_performance_all_stocks)
   end
+
+  private
+  def sum_for_each_month(performance_of_all_stocks)
+    monthly_values = performance_of_all_stocks.transpose
+    monthly_values.map do |monthly_value|
+      monthly_value.reduce(0) {|value, sum| value + sum}
+   end
+  end
+
 end
