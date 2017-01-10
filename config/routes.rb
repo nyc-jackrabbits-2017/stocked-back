6b5a6959ce902acc82f1df180596b117a7ad9ed4
Patrick Kolodgy <email": null>
2017-01-10 14:51:53 -0500

    Add api for destroy user

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show, :create, :update] do
+      resources :users, only: [:show, :create, :update, :destroy] do
         resources :purchased_stocks, only: [:index, :show]
         get 'last_year_portfolio_performance' => 'portfolio#last_year_portfolio_performance'
       end
